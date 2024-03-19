6c6a34c2262711df59fae0ceb4d19d4dde564b3b
Shivam Chaubey <shivam@Shivams-MacBook-Air-2.local>
2024-03-19 15:16:12 +0530

    resolved

--- scriptblue.py
+++ scriptblue.py
@@ -17,6 +17,75 @@ def moveTo(x, y, Pirate):
     else:
         return (position[1] < y) * 2 + 1
 
+def checkfriends(pirate , quad ):
+    sum = 0 
+    up = pirate.investigate_up()[1]
+    # print(up)
+    down = pirate.investigate_down()[1]
+    left = pirate.investigate_left()[1]
+    right = pirate.investigate_right()[1]
+    ne = pirate.investigate_ne()[1]
+    nw = pirate.investigate_nw()[1]
+    se = pirate.investigate_se()[1]
+    sw = pirate.investigate_sw()[1]
+    
+    if(quad=='ne'):
+        if(up == 'friend'):
+            sum +=1 
+        if(ne== 'friend'):
+            sum +=1 
+        if(right == 'friend'):
+            sum +=1 
+    if(quad=='se'):
+        if(down == 'friend'):
+            sum +=1 
+        if(right== 'friend'):
+            sum +=1 
+        if(se == 'friend'):
+            sum +=1 
+    if(quad=='sw'):
+        if(down == 'friend'):
+            sum +=1 
+        if(sw== 'friend'): 
+            sum +=1 
+        if(left == 'friend'):
+            sum +=1 
+    if(quad=='nw'):
+        if(up == 'friend'):
+            sum +=1 
+        if(nw == 'friend'):
+            sum +=1 
+        if(left == 'friend'):
+            sum +=1 
+
+    return sum
+    
+def spread(pirate):
+    sw = checkfriends(pirate ,'sw' )
+    se = checkfriends(pirate ,'se' )
+    ne = checkfriends(pirate ,'ne' )
+    nw = checkfriends(pirate ,'nw' )
+   
+    my_dict = {'sw': sw, 'se': se, 'ne': ne, 'nw': nw}
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+
+    x, y = pirate.getPosition()
+    
+    if( x == 0 , y == 0):
+        return randint(1,4)
+    
+    if(sorted_dict[list(sorted_dict())[3]] == 0 ):
+        return randint(1,4)
+    
+    if(list(sorted_dict())[0] == 'sw'):
+        return moveTo(x-1 , y+1 , pirate)
+    elif(list(sorted_dict())[0] == 'se'):
+        return moveTo(x+1 , y+1 , pirate)
+    elif(list(sorted_dict())[0] == 'ne'):
+        return moveTo(x+1 , y-1 , pirate)
+    elif(list(sorted_dict())[0] == 'nw'):
+        return moveTo(x-1 , y-1 , pirate)
+
 #will return the island number and the centre coordinates of the island
 def format_number(num):
     if num < 10:
@@ -135,87 +204,91 @@ def islandNameAndCoord(pirate):
     if ((down=='wall' or down=='blank') and (se=='wall' or ne=='blank') and (right=='wall' or right=='blank') and (nw=='wall' or nw=='blank') and (up=='island3') and (sw=='wall' or nw=='blank') and (left=='wall' or left=='blank') and (ne=='island3')):
         return [3, [format_number(x+1), format_number(y-2)]]
 
-           
+def setIslandCoordinatesToTeamSignal(pirate):
+    if (islandNameAndCoord(pirate)!=None):
+        raw_string = pirate.getTeamSignal()
+        island_number = int(islandNameAndCoord(pirate)[0])
+        strinG = ''.join(islandNameAndCoord(pirate)[1])
+        new_string = raw_string[:(island_number-1)*4] + strinG + raw_string[island_number*4:]
+        pirate.setTeamSignal(new_string)           
 
-Mdim= [40,40]
-# island_coord1 = [0,0]
-# island_coord2 = [0,0]
-# island_coord3 = [0,0]
-
-def checkfriends(pirate , quad ):
-    sum = 0 
-    up = pirate.investigate_up()[1]
-    #print(up)
-    down = pirate.investigate_down()[1]
-    left = pirate.investigate_left()[1]
-    right = pirate.investigate_right()[1]
-    ne = pirate.investigate_ne()[1]
-    nw = pirate.investigate_nw()[1]
-    se = pirate.investigate_se()[1]
-    sw = pirate.investigate_sw()[1]
+# def checkfriends(pirate , quad ):
+#     sum = 0 
+#     up = pirate.investigate_up()[1]
+#     #print(up)
+#     down = pirate.investigate_down()[1]
+#     left = pirate.investigate_left()[1]
+#     right = pirate.investigate_right()[1]
+#     ne = pirate.investigate_ne()[1]
+#     nw = pirate.investigate_nw()[1]
+#     se = pirate.investigate_se()[1]
+#     sw = pirate.investigate_sw()[1]
     
-    if(quad=='ne'):
-        if(up == 'friend'):
-            sum +=1 
-        if(ne== 'friend'):
-            sum +=1 
-        if(right == 'friend'):
-            sum +=1 
-    if(quad=='se'):
-        if(down == 'friend'):
-            sum +=1 
-        if(right== 'friend'):
-            sum +=1 
-        if(se == 'friend'):
-            sum +=1 
-    if(quad=='sw'):
-        if(down == 'friend'):
-            sum +=1 
-        if(sw== 'friend'): 
-            sum +=1 
-        if(left == 'friend'):
-            sum +=1 
-    if(quad=='nw'):
-        if(up == 'friend'):
-            sum +=1 
-        if(nw == 'friend'):
-            sum +=1 
-        if(left == 'friend'):
-            sum +=1 
+#     if(quad=='ne'):
+#         if(up == 'friend'):
+#             sum +=1 
+#         if(ne== 'friend'):
+#             sum +=1 
+#         if(right == 'friend'):
+#             sum +=1 
+#     if(quad=='se'):
+#         if(down == 'friend'):
+#             sum +=1 
+#         if(right== 'friend'):
+#             sum +=1 
+#         if(se == 'friend'):
+#             sum +=1 
+#     if(quad=='sw'):
+#         if(down == 'friend'):
+#             sum +=1 
+#         if(sw== 'friend'): 
+#             sum +=1 
+#         if(left == 'friend'):
+#             sum +=1 
+#     if(quad=='nw'):
+#         if(up == 'friend'):
+#             sum +=1 
+#         if(nw == 'friend'):
+#             sum +=1 
+#         if(left == 'friend'):
+#             sum +=1 
 
-    return sum
+#     return sum
     
-def spread(pirate):
-    sw = checkfriends(pirate ,'sw' )
-    se = checkfriends(pirate ,'se' )
-    ne = checkfriends(pirate ,'ne' )
-    nw = checkfriends(pirate ,'nw' )
+# def spread(pirate):
+#     sw = checkfriends(pirate ,'sw' )
+#     se = checkfriends(pirate ,'se' )
+#     ne = checkfriends(pirate ,'ne' )
+#     nw = checkfriends(pirate ,'nw' )
    
-    my_dict = {'sw': sw, 'se': se, 'ne': ne, 'nw': nw}
-    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+#     my_dict = {'sw': sw, 'se': se, 'ne': ne, 'nw': nw}
+#     sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
 
-    x, y = pirate.getPosition()
+#     x, y = pirate.getPosition()
     
-    if( x == 0 , y == 0):
-        return random.randint(1,4)
+#     if( x == 0 , y == 0):
+#         return random.randint(1,4)
     
-    if(sorted_dict[list(sorted_dict())[3]] == 0 ):
-        return random.randint(1,4)
+#     if(sorted_dict[list(sorted_dict())[3]] == 0 ):
+#         return random.randint(1,4)
     
-    if(list(sorted_dict())[0] == 'sw'):
-        return moveTo(x-1 , y+1 , pirate)
-    elif(list(sorted_dict())[0] == 'se'):
-        return moveTo(x+1 , y+1 , pirate)
-    elif(list(sorted_dict())[0] == 'ne'):
-        return moveTo(x+1 , y-1 , pirate)
-    elif(list(sorted_dict())[0] == 'nw'):
-        return moveTo(x-1 , y-1 , pirate)
+#     if(list(sorted_dict())[0] == 'sw'):
+#         return moveTo(x-1 , y+1 , pirate)
+#     elif(list(sorted_dict())[0] == 'se'):
+#         return moveTo(x+1 , y+1 , pirate)
+#     elif(list(sorted_dict())[0] == 'ne'):
+#         return moveTo(x+1 , y-1 , pirate)
+#     elif(list(sorted_dict())[0] == 'nw'):
+#         return moveTo(x-1 , y-1 , pirate)
 
 
 def ActPirate(pirate):
-    
-                
-    
+
+    if pirate.getTeamSignal() == '':
+        pirate.setTeamSignal("00000000000000000000000000000000")
+
+    setIslandCoordinatesToTeamSignal(pirate)
+
     up = pirate.investigate_up()[0]
     down = pirate.investigate_down()[0]
     left = pirate.investigate_left()[0]
@@ -290,61 +363,61 @@ def ActPirate(pirate):
 
     if pirate.getID() == '1':
         return moveTo(20,20,pirate)
-    if (
-        (up == "island1" and s[0] != "myCaptured")
-        or (up == "island2" and s[1] != "myCaptured")
-        or (up == "island3" and s[2] != "myCaptured")
-    ):  
-        if(up == "island1"):
-            island_coord1 = [x,y-1]
-        elif(up == "island2"):
-            island_coord2 = [x,y-1]
-        elif(up == "island3"):
-            island_coord3 = [x,y-1]
-        s = up[-1] + str(x) + "," + str(y - 1)
-        pirate.setTeamSignal(s)
+    # if (
+    #     (up == "island1" and s[0] != "myCaptured")
+    #     or (up == "island2" and s[1] != "myCaptured")
+    #     or (up == "island3" and s[2] != "myCaptured")
+    # ):  
+    #     if(up == "island1"):
+    #         island_coord1 = [x,y-1]
+    #     elif(up == "island2"):
+    #         island_coord2 = [x,y-1]
+    #     elif(up == "island3"):
+    #         island_coord3 = [x,y-1]
+    #     s = up[-1] + str(x) + "," + str(y - 1)
+    #     pirate.setTeamSignal(s)
 
-    if (
-        (down == "island1" and s[0] != "myCaptured")
-        or (down == "island2" and s[1] != "myCaptured")
-        or (down == "island3" and s[2] != "myCaptured")
-    ):
-        if(down == "island1"):
-            island_coord1 = [x,y-1]
-        elif(down == "island2"):
-            island_coord2 = [x,y-1]
-        elif(down == "island3"):
-            island_coord3 = [x,y-1]
-        s = down[-1] + str(x) + "," + str(y + 1)
-        pirate.setTeamSignal(s)
+    # if (
+    #     (down == "island1" and s[0] != "myCaptured")
+    #     or (down == "island2" and s[1] != "myCaptured")
+    #     or (down == "island3" and s[2] != "myCaptured")
+    # ):
+    #     if(down == "island1"):
+    #         island_coord1 = [x,y-1]
+    #     elif(down == "island2"):
+    #         island_coord2 = [x,y-1]
+    #     elif(down == "island3"):
+    #         island_coord3 = [x,y-1]
+    #     s = down[-1] + str(x) + "," + str(y + 1)
+    #     pirate.setTeamSignal(s)
 
-    if (
-        (left == "island1" and s[0] != "myCaptured")
-        or (left == "island2" and s[1] != "myCaptured")
-        or (left == "island3" and s[2] != "myCaptured")
-    ):
-        if(left == "island1"):
-            island_coord1 = [x,y-1]
-        elif(left == "island2"):
-            island_coord2 = [x,y-1]
-        elif(left == "island3"):
-            island_coord3 = [x,y-1]
-        s = left[-1] + str(x - 1) + "," + str(y)
-        pirate.setTeamSignal(s)
+    # if (
+    #     (left == "island1" and s[0] != "myCaptured")
+    #     or (left == "island2" and s[1] != "myCaptured")
+    #     or (left == "island3" and s[2] != "myCaptured")
+    # ):
+    #     if(left == "island1"):
+    #         island_coord1 = [x,y-1]
+    #     elif(left == "island2"):
+    #         island_coord2 = [x,y-1]
+    #     elif(left == "island3"):
+    #         island_coord3 = [x,y-1]
+    #     s = left[-1] + str(x - 1) + "," + str(y)
+    #     pirate.setTeamSignal(s)
 
-    if (
-        (right == "island1" and s[0] != "myCaptured")
-        or (right == "island2" and s[1] != "myCaptured")
-        or (right == "island3" and s[2] != "myCaptured")
-    ):
-        if(right == "island1"):
-            island_coord1 = [x,y-1]
-        elif(right == "island2"):
-            island_coord2 = [x,y-1]
-        elif(right == "island3"):
-            island_coord3 = [x,y-1]
-        s = right[-1] + str(x + 1) + "," + str(y)
-        pirate.setTeamSignal(s)
+    # if (
+    #     (right == "island1" and s[0] != "myCaptured")
+    #     or (right == "island2" and s[1] != "myCaptured")
+    #     or (right == "island3" and s[2] != "myCaptured")
+    # ):
+    #     if(right == "island1"):
+    #         island_coord1 = [x,y-1]
+    #     elif(right == "island2"):
+    #         island_coord2 = [x,y-1]
+    #     elif(right == "island3"):
+    #         island_coord3 = [x,y-1]
+    #     s = right[-1] + str(x + 1) + "," + str(y)
+    #     pirate.setTeamSignal(s)
 
     # print(island_coord1)
     # print(island_coord2)
@@ -370,10 +443,10 @@ def ActPirate(pirate):
         x = int(l[0][1:])
         y = int(l[1])
     
-        return moveTo(x, y, pirate)
+    #     return moveTo(x, y, pirate)
 
-    else:
-        return random.randint(1, 4)
+    # else:
+    return random.randint(1, 4)
 
 
 def ActTeam(team):
@@ -382,17 +455,23 @@ def ActTeam(team):
     Mdim = (xdim, ydim)
     l = team.trackPlayers()
     s = team.getTeamSignal()
-
+    
     alive_players=[]
-    print(team.getListOfSignals())
+    # print(team.getListOfSignals())
     # if team.getListOfSignals() !=['', '', '', '', '', '', '', '']:
     for string in team.getListOfSignals():
         lists = string.split()
-        # print(lists)
         if lists != []:
-            alive_players.append(lists[0])
+            strl= lists[0]
+            alive_players.append(strl[1:3])
             # print(lists[0])
     
+    def_alive= alive_players[:18]
+    sig_alive = ''.join(def_alive)
+    # print(sig_alive)
+    raw_string = team.getTeamSignal()
+    new_string = raw_string[:12] + sig_alive + raw_string[48:]
+    team.setTeamSignal(new_string)
     # print(team.getListOfSignals())
     # print(alive_players)    
     # print(team.getListOfSignals())
@@ -400,10 +479,10 @@ def ActTeam(team):
     team.buildWalls(1)
     team.buildWalls(2)
     team.buildWalls(3)
-    # print(team.getTeamSignal())
+    print(team.getTeamSignal())
     # print(team.trackPlayers())
-    if s:
-        island_no = int(s[0])
-        signal = l[island_no - 1]
-        if signal == "myCaptured":
-            team.setTeamSignal("")
+    # if s:
+    #     island_no = int(s[0])
+    #     signal = l[island_no - 1]
+    #     if signal == "myCaptured":
+    #         team.setTeamSignal("")
