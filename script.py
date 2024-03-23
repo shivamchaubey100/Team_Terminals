7f6bb4aba616f34a09b35e22c3d6c7e151f11e1b
Tejas <tejashns169@gmail.com>
2024-03-23 13:49:59 +0530

    minor changes in denfence function; still not working properly

--- script.py
+++ script.py
@@ -269,19 +269,19 @@ def ActTeam(team):
         team.setTeamSignal(string)
     
     #below block of code (3 if statements) will set the team signal index 48,49 and 50 to the islands which we have captured or are attempting to capture    
-    if(team.getTeamSignal()[48]==0) :
+    if(team.getTeamSignal()[48]=='0') :
         if (team.trackPlayers()[0]=='myCaptured' or team.trackPlayers()[0]=='myCapturing'):
             string = team.getTeamSignal()
             sig = string[0:48] + '1' + string[49:100] 
             team.setTeamSignal(sig)
 
-    if(team.getTeamSignal()[49]==0) :
+    if(team.getTeamSignal()[49]=='0') :
         if (team.trackPlayers()[1]=='myCaptured' or team.trackPlayers()[1]=='myCapturing'):
             string = team.getTeamSignal()
             sig = string[0:49] + '2' + string[50:100] 
             team.setTeamSignal(sig)    
 
-    if(team.getTeamSignal()[50]==0) :
+    if(team.getTeamSignal()[50]=='0') :
         if (team.trackPlayers()[2]=='myCaptured' or team.trackPlayers()[2]=='myCapturing'):
             string = team.getTeamSignal()
             sig = string[0:50] + '3' + string[51:100] 
